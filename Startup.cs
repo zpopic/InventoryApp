0e06e2374ddc2d1d7fa25e82403792b333409a2e
Zlatko Popić <zlatko_popic@hotmail.com>
2020-08-23 23:28:54 +0200

    - instalacija i integraija Admin LTE po boljoj metodi: https://www.codewithmukesh.com/blog/integrating-adminlte-with-aspnet-core/

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,8 @@ namespace InventoryApp
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+            
         }
 
         public IConfiguration Configuration { get; }
@@ -53,6 +55,10 @@ namespace InventoryApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -64,6 +70,11 @@ namespace InventoryApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+
+
+
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
